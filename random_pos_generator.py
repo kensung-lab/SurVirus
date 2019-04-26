4c5d705db163292b191efa3d183f278e15c766ec
Ramesh Rajaby <ramesh@genome.comp.nus.edu.sg>
2019-04-26 15:33:31 +0800

    Fixed problem where the random position generator may randomly fail.

--- random_pos_generator.py
+++ random_pos_generator.py
@@ -1,13 +1,13 @@
 from random import randint
 
-
 class RandomPositionGenerator:
     def __init__(self, sampling_regions):
         self.sampling_regions = sampling_regions
         self.reference_len = sum([r[2]-r[1] for r in sampling_regions])
 
     def next(self):
-        r = randint(0, self.reference_len)
+        # according to documentation this is supposed to generate within [a, b), but sometimes it generates b for me
+        r = randint(0, self.reference_len-1)
         for region in self.sampling_regions:
             k, start, stop = region
             if r < stop-start:
